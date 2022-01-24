f064026a9a68c01e5b068163ccff973c6836d644
Cristian Malacarne Huff <cristian.huff@rodosoft.com.br>
2022-01-24 13:57:32 -0300

    Iniciando o projeto Auluracord da semana de imersão react da Alura

---
+++ Pages/index.js
@@ -0,0 +1,16 @@
+function HomePage() {
+    return (
+    <div>
+        <h1>Boas vinda de volta!</h1>
+        <h2>Discord - Alura Matrix</h2>
+         <style jsx>{`
+       {
+        color: red;
+      }
+    `}</style>
+
+    </div>
+    )
+  }
+  
+  export default HomePage
